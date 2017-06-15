206885a89d8e705ec14966ae60317f4976bb41f6
hx <781742009@qq.com>
2017-06-15 15:45:31 +0800

    num
    冒泡排序

--- num/main.c
+++ num/main.c
@@ -1,15 +1,41 @@
-//
-//  main.c
-//  num
-//
-//  Created by 20161104601 on 17/6/13.
-//  Copyright © 2017年 20161104601. All rights reserved.
-//
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+#include<stdio.h>
+int main()
+{
+    int a[10];
+    int i,j,t,temp;//temp记录临时中间值
+    FILE *fr;
+    FILE *fw;
+    fw=fopen("//Users//a20161104601//Desktop//num//output","w+");
+    fr=fopen("//Users//a20161104601//Desktop//num//input","r+");
+    for (i=0;i<10;i++)
+    {
+        fscanf(fr,"%d",&a[i]);
+    }
+    for (j=1;j<=9;j++)
+    {
+        t=10-j;
+        for (i=0;i<t;i++)
+        {
+            if (a[i]>a[i+1]) {
+                
+                // 交换两数的位置
+                
+                temp=a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;
+            }
+        }
+        
+    }
+    for (i=0;i<=9;i++)
+    {
+        printf("%d ",a[i]);
+        fprintf(fw,"%d ",a[i]);
+    }
+    printf("\n");
+    fprintf(fw,"\n");
     return 0;
 }
+
+
